8d09a11fcc24e62d1f8a05f563da333df1b732aa
NAV1R4IN <songziqi1998@gmail.com>
2019-09-26 21:25:51 +0800

    20190926

--- 2019ACM/gym101955c.cpp
+++ 2019ACM/gym101955c.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-	freopen("in","r",stdin);
+	freopen("in", "r", stdin);
 	freopen("out", "w", stdout);
-	int tt;
-	scanf("%d", &tt);
-	printf("%d", tt);
+	
 	return 0;
 }
