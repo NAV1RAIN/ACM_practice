db4de37bf57f4e6d702f8d9e1473e3d67439e9f5
NAV1R4IN <songziqi1998@gmail.com>
2019-10-09 20:37:11 +0800

    20191009

--- 2019ACM/Forest Program/main.cpp
+++ 2019ACM/Forest Program/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD = 998244353;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m;
+    while(~scanf("%d%d", &n, &m)) {
+        for(int i = 0; i < m; i++) {
+            int a, b;
+            scanf("%d%d", &a, &b);
+        }
+    }
     return 0;
 }
