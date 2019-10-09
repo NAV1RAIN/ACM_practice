55da7c4c64284ed541bc443a544136968df57f0d
NAV1R4IN <songziqi1998@gmail.com>
2019-10-09 20:26:42 +0800

    20191009

---
+++ 2019ACM/Forest Program/cmake-build-debug/CMakeFiles/Forest_Program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Forest_Program.dir/main.cpp.o"
+  "Forest_Program"
+  "Forest_Program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Forest_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2019ACM/Forest Program/cmake-build-debug/CMakeFiles/Forest_Program.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Forest_Program.
+# This may be replaced when dependencies are built.
