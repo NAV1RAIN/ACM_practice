bf7f362379a907f1351e161f6afc503c98626e20
NAV1RAIN <songziqi1998@gmail.com>
2019-10-11 14:08:07 +0800

    20191011

--- 2019ACM/Forest Program/main.cpp
+++ 2019ACM/Forest Program/main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MOD = 998244353;
+typedef long long LL;
+const LL MOD = 998244353;
+const int SN = 3e3+5;
+const int SM = 5e5+5;
+void DFS() {
+
+}
 int main() {
     int n, m;
     while(~scanf("%d%d", &n, &m)) {
