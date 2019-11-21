bbddf0e728589baf4a017ba224580b344516412f
NAV1RAIN <songziqi1998@gmail.com>
2019-11-21 09:55:44 +0800

    20191121

--- 2019ACM/cf1255c.cpp
+++ 2019ACM/cf1255c.cpp
@@ -4,6 +4,7 @@ const int SIZE = 1e5+5;
 struct Node {
     int a, b, c;
 }node[SIZE];
+vector<Node> pl;
 int main() {
     int n;
     scanf("%d", &n);
@@ -12,7 +13,7 @@ int main() {
         scanf("%d%d%d", &a, &b, &c);
         node[i].a = a, node[i].b = b, node[i].c = c;
     }
-    int jl = -1;
+    int jl = 0;
     for(int i = 0; i < n-1; i++) {
         set<int> ss;
         ss.insert(node[i].a);
@@ -22,10 +23,13 @@ int main() {
         ss.insert(node[i+1].b);
         ss.insert(node[i+1].c);
         int sLen = ss.size();
-        if(sLen > 4) jl = i;
+        if(sLen > 4) jl = i+1;
     }
-    for(int i = jl; i < n-1; i++) {
-        
+    for(int i = jl; i < n; i++) {
+        pl.push_back(node[i]);
+    }
+    for(int i = 0; i < jl; i++) {
+        pl.push_back(node[i]);
     }
     return 0;
 }
